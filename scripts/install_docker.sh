854bca5742590eb2bb70c3b93ac3c9ffd9ec1d1c
Eduardo A. Paris Penas <edward2a@gmail.com>
2018-10-04 16:49:32 +0100

    add static login (temporary workaround)

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -6,3 +6,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get -qq install docker-ce
+
+if [ -f $(dirname $0)/docker.login ]; then
+    source docker.login
+    docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} ${DOCKER_REGISTRY}
+fi
