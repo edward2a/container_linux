6c681dab06b749979450f2935a7b4d413cb7d1e5
Eduardo A. Paris Penas <edward2a@gmail.com>
2018-10-18 18:26:18 +0100

    add dir for offset tracking sqlite dbs for fluentbit; update rsyslog to output syslog protocol 23 (prior, similar to rfc5424) by default

--- scripts/container_linux_setup.sh
+++ scripts/container_linux_setup.sh
@@ -15,8 +15,25 @@ function install_fluentbit(){
     echo "deb http://packages.fluentbit.io/ubuntu xenial main" > /etc/apt/sources.list.d/fluentbit.list
     apt-get update
     apt-get install ${APT_OPTS} td-agent-bit
+
+    # file input db cache location
+    mkdir /var/cache/td-agent-bit
+}
+
+function configure_rsyslog(){
+
+    # Use syslog protocol 23 (similar to RFC5424)
+    sed -i -e 's/^\$ActionFileDefaultTemplate RSYSLOG_TraditionalFileFormat/$ActionFileDefaultTemplate RSYSLOG_SyslogProtocol23Format/' /etc/rsyslog.conf
+
+    if grep -q '$ActionFileDefaultTemplate RSYSLOG_SyslogProtocol23Format' /etc/rsyslog.conf; then
+        return 0
+    else
+        echo "ERROR: failed to configure rsyslogd!"
+        return 1
+    fi
 }
 
 
 # MAIN
 install_fluentbit
+configure_rsyslog
